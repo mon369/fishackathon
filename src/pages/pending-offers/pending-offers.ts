65953bdbf2b5dba208ce83f314ae09558c110738
Abel Simon Inocencio <simoninoc@outlook.com>
2018-02-17 18:19:02 -0500

    Pending offers UI changes

--- src/pages/pending-offers/pending-offers.ts
+++ src/pages/pending-offers/pending-offers.ts
@@ -24,7 +24,8 @@ export class PendingOffersPage {
     weight: number,
     price: number,
     location: string,
-    port : string
+    port: string,
+    dateOffered: string
   }[];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -34,67 +35,25 @@ export class PendingOffersPage {
     this.pendingOffers = [
       {
         "id": 0,
-        "name": "Yellowfish Tuna",
+        "name": "Yellowfin Tuna",
         "weight": 100,
         "img": "assets/imgs/fishes/yellowfish_tuna.jpg",
         "price": 0.40,
-        "location" : "Fiji",
-        "port": "Rotuma"
-      },
-      {
-        "id": 1,
-        "name": "Large Bigeye",
-        "img": "assets/imgs/fishes/large_bigeye.jpg",
-        "weight": 120,
-        "price": 0.49,
-        "location" : "Fiji",
-        "port" : "Suva"
-      },
-      {
-        "id": 2,
-        "name": "Large Bigeye",
-        "img": "assets/imgs/fishes/large_bigeye.jpg",
-        "weight":80,
-        "price": 0.25,
-        "location" : "Fiji",
-        "port" : "Levuka"
-      },
-      {
-        "id": 3,
-        "name": "Yellowfish Tuna",
-        "weight": 150,
-        "img": "assets/imgs/fishes/yellowfish_tuna.jpg",
-        "price": 1.29,
-        "location" : "Fiji",
-        "port" : "Rotuna"
-      }, 
-      {
-        "id": 4,
-        "name": "Swordfish",
-        "img": "assets/imgs/fishes/swordfish.jpg",
-        "weight": 140,
-        "price": 0.30,
-        "location" : "Fiji",
-        "port" : "Suva"
-      }, 
-      {
-        "id": 5,
-        "name": "Blue Marlin",
-        "img": "assets/imgs/fishes/blue_marlin.jpg",
-        "weight": 140,
-        "price": 0.65,
-        "location" : "Fiji",
-        "port" : "Suva"
-      },
+        "location": "Fiji",
+        "port": "Rotuma",
+        "dateOffered": "February 17, 2018"
+      }
+      ,
       {
         "id": 6,
         "name": "Spotted Sunfish",
         "img": "assets/imgs/fishes/spotted_sunfish.jpg",
         "weight": 130,
         "price": 0.25,
-        "location" : "Fiji",
-        "port" : "Levuka"
-      }
+        "location": "Fiji",
+        "port": "Levuka",
+        "dateOffered": "February 17, 2018"
+       }
     ]
   }
   viewOffer(offerData: any) {
